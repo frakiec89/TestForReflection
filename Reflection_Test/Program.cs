3244d78bd7a24aea836e90ee18bd8646d484cc3a
РИШАТ Ахтямов <rishat-rus@mail.ru>
2021-08-25 13:25:43 +0400

    Добавьте файлы проекта.

---
+++ Reflection_Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Reflection_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Assembly asm = Assembly.
+                LoadFrom(@"C:\Users\Rishat\source\repos\TestForReflection\TestForReflection\bin\Debug\netcoreapp3.1\TestForReflection.dll");
+            Console.WriteLine("Вы в программе  для входа в систему");
+
+           
+            Console.WriteLine(asm.FullName);
+            Console.WriteLine("В сборке вот такие классы");
+            // получаем все типы из сборки dll
+            List<Type> types = asm.GetTypes().ToList();
+            foreach (Type t in types)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+            Type pr = types[0];
+
+            // создаем экземпляр класса Program
+            object obj = Activator.CreateInstance(pr);
+            
+            Console.WriteLine("напишите  \'да\' чтобы зайти  через ввод пароля");
+
+            Console.WriteLine("если  хотите  обойти  ввод  пароля напишите \'РЕФЛЕКСИЯ\'");
+
+            string temp = Console.ReadLine().ToLower();
+
+            if ( temp == "да" )
+            {
+                // вызов метода  мейн 
+                MethodInfo method = pr.GetMethod("Main", BindingFlags.DeclaredOnly
+        |       BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+                method.Invoke(obj, new object[] { new string[] { } });
+            }
+
+            if (temp == "рефлексия")
+            {
+                MethodInfo method = pr.GetMethod("GetStart", BindingFlags.DeclaredOnly |
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+                method.Invoke(obj, null);
+            }
+        }
+    }
+}
---
+++ TestForReflection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TestForReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Ivan", "admin", "123");
+            while(true)
+            {
+                Console.WriteLine("Введите пароль");
+
+                if (user.IsAuthorization(Console.ReadLine()))
+                {
+                    Console.WriteLine("Hello " + user.Name);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("пароль не верный");
+                }
+            }
+            GetStart();
+        }
+
+        private static void GetStart()
+        {
+            Console.WriteLine(" Вы в  системе - Ура");
+            Console.ReadLine();
+        }
+    }
+
+    class User
+    {
+        public string Name { get; private set; }
+        public string Login { get; private set; }
+
+        private string password;
+
+        public User(string name, string login, string password)
+        {
+            Name = name;
+            Login = login;
+            this.password = password;
+        }
+
+        public bool IsAuthorization ( string pass)
+        {
+            if (password == pass)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
